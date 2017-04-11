9f72deeacb2b4b5e5a7a0cea9c83fccc00432a8d
naokiur <atomic.transfer.comp@gmail.com>
2017-04-11 23:58:15 +0900

    Create package for Iterator pattern.

--- src/main/java/jp/ne/naokiur/design/pattern/iterator/ItelatorSample.java
+++ src/main/java/jp/ne/naokiur/design/pattern/iterator/ItelatorSample.java
@@ -1,5 +1,13 @@
 package jp.ne.naokiur.design.pattern.iterator;
 
 public class ItelatorSample {
+    private String name;
+    private String address;
+
+    public ItelatorSample(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
 
 }
