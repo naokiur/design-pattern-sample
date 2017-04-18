c74a682c8e780886130d8bf34724483011a00d51
naokiur <atomic.transfer.comp@gmail.com>
2017-04-18 21:03:04 +0900

    Modify target data.

--- src/main/java/jp/ne/naokiur/design/pattern/iterator/App.java
+++ src/main/java/jp/ne/naokiur/design/pattern/iterator/App.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) {
-        // These codes is wrong pattern according to OOP begin.
+        List<Employee> targetEmployees = Arrays.asList(new Employee("john", "John", "Smith", 24), new Employee("jane", "Jane", "Smith", 22), new Employee("bob", "Bob", "Smith", 22));
+
+        // ## These codes is wrong pattern according to OOP begin.
         // I think that the top of reason is if we need to modify the method,
         // we need to modify App class despite we would not like to modify App class.
-        List<Employee> employees = Arrays.asList(new Employee("john", "John", "Smith", 24), new Employee("jane", "Jane", "Smith", 22), new Employee("bob", "Bob", "Smith", 22));
+        List<Employee> employees = targetEmployees;
 
         // This is assumption of Requirement that We need to search first of target age employee.
         Employee firstMatchedEmployee = searchTargetAgeEmployeeFirstly(employees, 22);
@@ -21,8 +23,8 @@ public class App {
         System.out.println("Wrong pattern of Last:" + lastMatchedEmployee.getFirstName());
         // These codes is wrong pattern according to OOP end.
 
-        // Try to Iterator pattern begin.
-        EmployeeMember employeeMember = new EmployeeMember(Arrays.asList(new Employee("john", "John", "Smith", 24), new Employee("jane", "Jane", "Smith", 22), new Employee("bob", "Bob", "Smith", 22)));
+        // ## Try to Iterator pattern begin.
+        EmployeeMember employeeMember = new EmployeeMember(targetEmployees);
         Iterator iterator = employeeMember.iterator();
         while (!iterator.isMatchedAge(22)) {
             iterator.next();
@@ -30,7 +32,6 @@ public class App {
         Employee matchedEmployee = iterator.current();
         System.out.println("Correct pattern of first:" + matchedEmployee.getFirstName());
 //        System.out.println("Correct pattern of last:" + matchedEmployee.getFirstName());
-
 
         // Try to Iterator pattern end.
     }
