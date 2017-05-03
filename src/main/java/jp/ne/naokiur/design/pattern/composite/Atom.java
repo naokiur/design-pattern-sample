e980ef406d31599dae38f9b20df9d7e6a17f1d4e
naokiur <atomic.transfer.comp@gmail.com>
2017-05-03 18:40:17 +0900

    Fix createChemicalFormula().

--- src/main/java/jp/ne/naokiur/design/pattern/composite/Atom.java
+++ src/main/java/jp/ne/naokiur/design/pattern/composite/Atom.java
@@ -1,7 +1,9 @@
 package jp.ne.naokiur.design.pattern.composite;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class Atom {
     private String elementSynbol;
@@ -18,12 +20,45 @@ public abstract class Atom {
     }
 
     public String createChemicalFormula() {
-        String result = this.elementSynbol;
+//        String result = this.elementSynbol;
+        Map<String, Integer> elementCount = new HashMap<>();
+        elementCount.put(this.elementSynbol, 1);
+
         for (Atom atom : bonds) {
-            result = result + atom.createChemicalFormula();
+            Map<String, Integer> bondElementCount = atom.countElement();
+
+            for (Map.Entry<String, Integer> entry : bondElementCount.entrySet()) {
+                if (elementCount.containsKey(entry.getKey())) {
+                    Integer count = elementCount.get(entry.getKey()) + entry.getValue();
+                    elementCount.put(entry.getKey(), count);
+                } else {
+                    elementCount.put(entry.getKey(), 1);
+
+                }
+            }
         }
 
+        String result = "";
+        for (Map.Entry<String, Integer> element : elementCount.entrySet()) {
+            result = result + element.getKey() + element.getValue();
+        }
 
         return result;
+    }
+
+    private Map<String, Integer> countElement() {
+        Map<String, Integer> elementCount = new HashMap<>();
+        elementCount.put(this.elementSynbol, 1);
+        for (Atom atom : bonds) {
+            if (elementCount.containsKey(atom.getElementSynbol())) {
+                Integer count = elementCount.get(atom.getElementSynbol()) + 1;
+                elementCount.put(atom.getElementSynbol(), count);
+            } else {
+                elementCount.put(atom.getElementSynbol(), 1);
+
+            }
+        }
+
+        return elementCount;
     }
 }
